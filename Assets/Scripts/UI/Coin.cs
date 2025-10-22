dad04ff6cf5004d93b57b9c602c3f9a64222c0cb
yam-chay <yam234567@gmail.com>
2025-10-22 18:47:11 +0300

    bag
    - added bag gameobject
    - addeed coins script

---
+++ Assets/Scripts/UI/Coin.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace TDLogic
+{
+    public class Coin : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        private bool inBag;
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody2D>();
+        }
+        private void Start()
+        {    
+            inBag = false;
+            animator.SetBool("inBag", false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Bag") && !inBag)
+            {
+                transform.localScale /= 2f;
+                rb.linearVelocity = Vector2.zero;
+                inBag = true;
+            }
+        }
+    }
+}
